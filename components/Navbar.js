98642cea54542e7033691a5d9e2f29dbf3bbfa7c
AdityaSawant0912 <csi.sakec2022@gmail.com>
2022-12-14 18:05:31 +0530

    footer changes

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,4 +1,5 @@
 import Link from 'next/link';
+import Image from 'next/image';
 
 const Navbar = () => {
 	return (
@@ -7,12 +8,12 @@ const Navbar = () => {
 				<div className=" navbar navbar-expand-lg navbar-light row ">
 					<div className="col col-lg-2">
 						<div className="navbar-brand row">
-							<a className=" col" href="https://www.idf.org.in">
-								<img
+							<Link className=" col" href="https://www.idf.org.in">
+								<Image
 									src="https://static.wixstatic.com/media/d3cf27_7565cf50f68f4fe2858ef7f67371d7aa~mv2.png/v1/fill/w_46,h_71,al_c,q_85,usm_0.66_1.00_0.01,enc_auto/idf-logo.png"
 									alt="idf-logo.png"
 								/>
-							</a>
+							</Link>
 							<div className=" col col-lg-2">
 								<h1 className="row ">Indian Development Foundation</h1>
 								<p className="row nav-text">
@@ -25,12 +26,12 @@ const Navbar = () => {
 							</div>
 						</div>
 						<div class="navbar-nav">
-							<a class="nav-link" aria-current="page" href="../">
+							<Link class="nav-link" aria-current="page" href="../">
 								Home
-							</a>
-							<a class="nav-link" href="../balgurukul">
+							</Link>
+							<Link class="nav-link" href="../balgurukul">
 								Balgurukuls
-							</a>
+							</Link>
 						</div>
 					</div>
 				</div>
