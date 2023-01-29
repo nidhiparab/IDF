643145185e0b0d4712eee9f94d3ee56083b60617
AdityaSawant0912 <arsawant09@gmail.com>
2023-01-30 00:26:20 +0530

    grade insert working

--- pages/grade/create/[bg_id].js
+++ pages/grade/create/[bg_id].js
@@ -3,47 +3,7 @@ import baseUrl from '../../../helpers/baseUrl';
 import gradeConstants from '../../../lib/grades';
 import styles from '../../../styles/Grade.module.css';
 import { useState } from 'react';
-import { useFormik } from 'formik';
-
-const OptionComponent = ({ options, value, onChange, isVisible }) => {
-  let gradeOptions = gradeConstants[value];
-  let columns = gradeOptions.columns;
-  let grade_opt = gradeOptions.options;
-  if (!isVisible) return <></>
-  return (
-    <div>
-      {Object.keys(columns).map((key_col) => {
-        let value = columns[key_col];
-        return (<div key={key_col}>
-          <br />
-          <br />
-          <h4>{value}</h4>
-          <div>
-            <ul className={styles.options}>
-              {Object.keys(grade_opt).map((key_opt) => {
-                return (
-                  <li key={key_opt}>
-                    <input
-                      type="radio"
-                      name={key_col}
-                      value={grade_opt[key_opt]}
-                      checked={grade_opt[key_opt] == "Not Applicable" ? true : false}
-                      onChange={(e) => {
-                        e.preventDefault();
-
-                      }}
-                    />
-                    <label htmlFor={key_col}>{grade_opt[key_opt]} </label>
-                  </li>)
-              })}
-            </ul>
-          </div>
-
-        </div>)
-      })}
-    </div>
-  )
-}
+import { useFormik, Field, FormikProvider } from 'formik';
 
 
 const CreateGrade = ({ bg, students }) => {
@@ -58,54 +18,93 @@ const CreateGrade = ({ bg, students }) => {
   const formik = useFormik({
     initialValues: {
       studentDetails: {
-        student_id: '',
-        bg_id: '',
-        f_name: '',
-        m_name: '',
-        l_name: '',
-        dob: '',
-        gender: '',
-        grade: '',
+        student_id: selected.student_id,
+        bg_id: selected.bg_id,
+        exam: '',
+        f_name: selected.f_name,
+        m_name: selected.m_name,
+        l_name: selected.l_name,
+        dob: selected.dob,
+        gender: selected.gender,
+        grade: selected.grade,
       },
-      qualities: {
-        Q1: '',
-        Q2: '',
-        Q3: '',
-        Q4: '',
-        Q5: '',
-        Q6: '',
-        Q7: '',
-        Q8: '',
-        Q9: '',
-        Q10: '',
-        Q11: '',
-        Q12: ''
+      grade_qualities: {
+        Q1: 'O7',
+        Q2: 'O7',
+        Q3: 'O7',
+        Q4: 'O7',
+        Q5: 'O7',
+        Q6: 'O7',
+        Q7: 'O7',
+        Q8: 'O7',
+        Q9: 'O7',
+        Q10: 'O7',
+        Q11: 'O7',
+        Q12: 'O7'
       },
-      subjects: {
-        Q1: '',
-        Q2: '',
-        Q3: '',
-        Q4: '',
-        Q5: ''
+      grade_subjects: {
+        Q1: 'O9',
+        Q2: 'O9',
+        Q3: 'O9',
+        Q4: 'O9',
+        Q5: 'O9'
       },
-      intrests: {
-        Q1: '',
-        Q2: '',
-        Q3: '',
-        Q4: '',
-        Q5: '',
-        Q6: '',
-        Q7: '',
-        Q8: '',
-        Q9: ''
+      grade_intrests: {
+        Q1: 'O7',
+        Q2: 'O7',
+        Q3: 'O7',
+        Q4: 'O7',
+        Q5: 'O7',
+        Q6: 'O7',
+        Q7: 'O7',
+        Q8: 'O7',
+        Q9: 'O7',
       },
-      specifics: {
+      grade_specifics: {
         Q1: '',
         Q2: '',
         Q3: ''
       }
     },
+    onSubmit: async (values) => {
+      let grade_qualities = []
+      let grade_subjects = []
+      let grade_intrests = []
+      let grade_specifics = []
 
+      for (const [key, value] of Object.entries(values.grade_qualities)) {
+        grade_qualities.push(value)
+      }
+      for (const [key, value] of Object.entries(values.grade_subjects)) {
+        grade_subjects.push(value)
+      }
+      for (const [key, value] of Object.entries(values.grade_intrests)) {
+        grade_intrests.push(value)
+      }
+      for (const [key, value] of Object.entries(values.grade_specifics)) {
+        grade_specifics.push(value)
+      }
+
+      let res = await fetch(`${baseUrl}/api/student/grade/insert`, {
+        method: 'POST',
+        headers: {
+          'Accept': 'application/json',
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+          student_id: values.studentDetails.student_id,
+          bg_id: values.studentDetails.bg_id,
+          exam: values.studentDetails.exam,
+          grade: values.studentDetails.grade,
+          grade_qualities,
+          grade_subjects,
+          grade_intrests,
+          grade_specifics
+        })
+      })
+      let data = await res.json();
+      console.log(data);
+    }
   });
 
 
@@ -119,24 +118,10 @@ const CreateGrade = ({ bg, students }) => {
   }
 
 
-  const StudentDetails = ({ bg, student, isVisible }) => {
-    if (!isVisible) return <></>
-    return (
-      <div>
-        <label htmlFor="name">Name </label>
-        <input type="text" {...formik.getFieldProps('studentDetails.f_name')} />
-        <input type="text" {...formik.getFieldProps('studentDetails.m_name')} />
-        <input type="text" {...formik.getFieldProps('studentDetails.l_name')} />
-      </div>
-    )
-  };
-
-
-
-
 
   return (
-    <>
+    <FormikProvider value={formik}>
+
       <div className="flex flex-row justify-between m-5">
         <div className="flex flex-col p-20 border">Student
           <div>
@@ -155,28 +140,198 @@ const CreateGrade = ({ bg, students }) => {
             }
           </div>
         </div>
-        <div className="flex flex-col border pl-20 pr-20">Sections
-          <div className="flex flex-row">
-            <div className="flex flex-col border m-1 p-2" onClick={(e) => { changeTab(e, "studentDetails") }}>Student Details</div>
-            <div className="flex flex-col border m-1 p-2" onClick={(e) => { changeTab(e, "qualities") }}>Qualities</div>
-            <div className="flex flex-col border m-1 p-2" onClick={(e) => { changeTab(e, "intrests") }}>Intrests</div>
-            <div className="flex flex-col border m-1 p-2" onClick={(e) => { changeTab(e, "subjects") }}>Academics</div>
-            <div className="flex flex-col border m-1 p-2" onClick={(e) => { changeTab(e, "specifics") }}>Specifics</div>
-          </div>
-          {/* <OptionComponent isVisible={studentDetails} value={'grade_studentDetails'} ></OptionComponent> */}
-          {!studentDetails ? <></> :
-            <div>
-              <label htmlFor="name">Name </label>
-              <input type="text" {...formik.getFieldProps('studentDetails.f_name')} />
-              <input type="text" {...formik.getFieldProps('studentDetails.m_name')} />
-              <input type="text" {...formik.getFieldProps('studentDetails.l_name')} />
+        <form onSubmit={formik.handleSubmit}>
+          <div className="flex flex-col border pl-20 pr-20">Sections
+            <div className="flex flex-row">
+              <div className="flex flex-col border m-1 p-2" onClick={(e) => { changeTab(e, "studentDetails") }}>Student Details</div>
+              <div className="flex flex-col border m-1 p-2" onClick={(e) => { changeTab(e, "qualities") }}>Qualities</div>
+              <div className="flex flex-col border m-1 p-2" onClick={(e) => { changeTab(e, "intrests") }}>Intrests</div>
+              <div className="flex flex-col border m-1 p-2" onClick={(e) => { changeTab(e, "subjects") }}>Academics</div>
+              <div className="flex flex-col border m-1 p-2" onClick={(e) => { changeTab(e, "specifics") }}>Specifics</div>
             </div>
-          }
-          <OptionComponent isVisible={qualities} value={'grade_qualities'} ></OptionComponent>
-          <OptionComponent isVisible={subject} value={'grade_subjects'} ></OptionComponent>
-          <OptionComponent isVisible={intrests} value={'grade_intrests'} ></OptionComponent>
-          {/* <OptionComponent isVisible={specifics} value={'grade_specifics'} ></OptionComponent> */}
-        </div>
+            {/* <OptionComponent isVisible={studentDetails} value={'grade_studentDetails'} ></OptionComponent> */}
+            {!studentDetails ? <></> :
+              <div>
+                <input type="text" hidden {...formik.getFieldProps('studentDetails.student_id')} />
+                <input type="text" hidden {...formik.getFieldProps('studentDetails.bg_id')} />
+                <label htmlFor="name">Exam </label>
+                <br />
+                <input type="text" {...formik.getFieldProps('studentDetails.exam')} />
+                <br />
+                <label htmlFor="name">Name </label>
+                <br />
+                <input type="text" {...formik.getFieldProps('studentDetails.f_name')} />
+                <br />
+                <br />
+                <input type="text" {...formik.getFieldProps('studentDetails.m_name')} />
+                <br />
+                <br />
+                <input type="text" {...formik.getFieldProps('studentDetails.l_name')} />
+                <br />
+                <label htmlFor="grade">Grade</label>
+                <br />
+                <input type="text" {...formik.getFieldProps('studentDetails.grade')} />
+                <br />
+                <label htmlFor="dob">DOB</label>
+                <br />
+                <input type="text" {...formik.getFieldProps('studentDetails.dob')} />
+                <br />
+                <label htmlFor="gender">Gender</label>
+                <br />
+                <input type="text" {...formik.getFieldProps('studentDetails.gender')} />
+                <br />
+                <button onClick={(e) => { e.preventDefault(); setStudentDetails(false); setQualities(true) }}>
+                  Next
+                </button>
+              </div>
+            }
+            {(() => {
+              if (!qualities)
+                return (<></>)
+              else {
+                let obj = 'grade_qualities'
+                let gradeOptions = gradeConstants[obj];
+                let columns = gradeOptions.columns;
+                let grade_opt = gradeOptions.options;
+                return (
+                  <div>
+                    {Object.keys(columns).map((key_col) => {
+                      let value = columns[key_col];
+                      return (<div key={key_col}>
+                        <br />
+                        <br />
+                        <h4>{value}</h4>
+                        <div>
+                          <ul className={styles.options}>
+                            {Object.keys(grade_opt).map((key_opt) => {
+                              console.log("name, value, some");
+                              console.log(key_col, key_opt, value);
+                              return (
+                                <li key={key_opt}>
+                                  <Field type="radio" name={`${obj}.${key_col}`} value={key_opt}>
+                                  </Field>
+
+                                  <label htmlFor={key_col}>{grade_opt[key_opt]} </label>
+                                </li>)
+                            })}
+                          </ul>
+                        </div>
+
+                      </div>)
+                    })}
+                    <button onClick={(e) => { e.preventDefault(); setQualities(false); setSubject(true) }}>
+                      Next
+                    </button>
+                  </div>
+                )
+              }
+            })()}
+
+            {(() => {
+              if (!subject)
+                return (<></>)
+              else {
+                let obj = 'grade_subjects'
+                let gradeOptions = gradeConstants[obj];
+                let columns = gradeOptions.columns;
+                let grade_opt = gradeOptions.options;
+                return (
+                  <div>
+                    {Object.keys(columns).map((key_col) => {
+                      let value = columns[key_col];
+                      return (<div key={key_col}>
+                        <br />
+                        <br />
+                        <h4>{value}</h4>
+                        <div>
+                          <ul className={styles.options}>
+                            {Object.keys(grade_opt).map((key_opt) => {
+                              console.log("name, value, some");
+                              console.log(key_col, key_opt, value);
+                              return (
+                                <li key={key_opt}>
+                                  <Field type="radio" name={`${obj}.${key_col}`} value={key_opt}>
+                                  </Field>
+
+                                  <label htmlFor={key_col}>{grade_opt[key_opt]} </label>
+                                </li>)
+                            })}
+                          </ul>
+                        </div>
+
+                      </div>)
+                    })}
+                    <button onClick={(e) => { e.preventDefault(); setSubject(false); setIntrests(true) }}>
+                      Next
+                    </button>
+                  </div>
+                )
+              }
+            })()}
+            {(() => {
+              if (!intrests)
+                return (<></>)
+              else {
+                let obj = 'grade_intrests'
+                let gradeOptions = gradeConstants[obj];
+                let columns = gradeOptions.columns;
+                let grade_opt = gradeOptions.options;
+                return (
+                  <div>
+                    {Object.keys(columns).map((key_col) => {
+                      let value = columns[key_col];
+                      return (<div key={key_col}>
+                        <br />
+                        <br />
+                        <h4>{value}</h4>
+                        <div>
+                          <ul className={styles.options}>
+                            {Object.keys(grade_opt).map((key_opt) => {
+                              console.log("name, value, some");
+                              console.log(key_col, key_opt, value);
+                              return (
+                                <li key={key_opt}>
+                                  <Field type="radio" name={`${obj}.${key_col}`} value={key_opt}>
+                                  </Field>
+
+                                  <label htmlFor={key_col}>{grade_opt[key_opt]} </label>
+                                </li>)
+                            })}
+                          </ul>
+                        </div>
+
+                      </div>)
+                    })}
+                    <button onClick={(e) => { e.preventDefault(); setIntrests(false); setSpecifics(true) }}>
+                      Next
+                    </button>
+
+                  </div>
+                )
+              }
+            })()}
+            {/* <OptionComponent isVisible={specifics} value={'grade_specifics'} ></OptionComponent> */}
+            {!specifics ? <></> :
+              <div>
+                <label htmlFor="Q1">Is there any specific or extraordinary talent / quality in the child? Please specify. </label>
+                <br />
+                <input type="textarea" {...formik.getFieldProps('grade_specifics.Q1')} />
+                <br />
+                <label htmlFor="grade">Is there any specific or unusual challenge or problem faced by the child? Kindly explain.</label>
+                <br />
+                <input type="textarea" {...formik.getFieldProps('grade_specifics.Q2')} />
+                <br />
+                <label htmlFor="grade">Any specific action plan or suggestion with regard to the child? Please share.</label>
+                <br />
+                <input type="textarea" {...formik.getFieldProps('grade_specifics.Q3')} />
+                <br />
+                <button type='submit'>
+                  Submit
+                </button>
+              </div>
+            }
+          </div>
+        </form>
         <div className="flex flex-col mr-20 border p-10">All students
           {students?.map((student) => {
             return (<>
@@ -184,22 +339,12 @@ const CreateGrade = ({ bg, students }) => {
               <div className="p-1 m-2 border" key={student.student_id} onClick={(e) => {
                 e.preventDefault();
                 setSelected(student);
-                formData = {
-                  studentDetails: {
-                    student_id: selected._id,
-                    bg_id: selected.bg_id,
-                    grade: selected.grade,
-                    dob: selected.dob,
-                    gender: selected.gender,
-                    f_name: selected.f_name,
-                    m_name: selected.m_name,
-                    l_name: selected.l_name
-                  },
-                  qualities: {},
-                  subjects: {},
-                  intrests: {},
-                  specifics: {}
-                }
+                formik.setFieldValue('studentDetails.student_id', student.student_id);
+                formik.setFieldValue('studentDetails.bg_id', student.bg_id);
+                formik.setFieldValue('studentDetails.f_name', student.f_name);
+                formik.setFieldValue('studentDetails.m_name', student.m_name);
+                formik.setFieldValue('studentDetails.dob', student.dob);
+                formik.setFieldValue('studentDetails.grade', student.grade);
               }}>
 
                 <p className="text-blue-700"> {student.f_name} {student.m_name} {student.l_name}</p>
@@ -208,9 +353,12 @@ const CreateGrade = ({ bg, students }) => {
 
             )
           })}
+
+
+
         </div>
       </div>
-    </>
+    </FormikProvider>
   );
 }
 
