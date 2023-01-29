6d92c80d19f817537f0f09aecf55ea2960e261c2
Nidhi <nidhiparabmail@gmail.com>
2023-01-29 13:34:56 +0530

    all students

--- pages/profile/students.js
+++ pages/profile/students.js
@@ -14,7 +14,27 @@ const Students = ({ students }) => {
   console.log(students);
     return (
     <>
-        {students.map(student => { return (<>{ student.f_name } <br></br></>)  })}
+        {students.map(student => { return (<>
+                                           
+          <div className="m-20 p-10 items-center shadow-2xl shadow-slate-700 rounded-2xl">
+      <div className=' text-l'>
+          <div className='justify-between p-2'>
+            <h3 className='text-l font-bold text-blue-600  mt-auto mb-3'>Name</h3>
+            <span className='mt-auto mb-2'> {student?.f_name} {student?.m_name} {student?.l_name}</span>
+          </div>
+          <div className=' my-auto w-auto flex flex-col items-start justify-between p-2'>
+            <h3 className='text-l font-bold text-blue-600  mt-auto mb-3'>Class</h3>
+            <span className='mt-auto mb-2'>{student.grade}</span>
+          </div>
+          <div className=' my-auto w-auto flex flex-col items-start justify-between p-2'>
+            <h3 className='text-l font-bold text-blue-600 mt-auto mb-3'>Balgurukul</h3>
+            <span className='mt-auto mb-2'>bgk name</span>
+          </div>
+          
+          </div>
+          </div>
+                                           
+                                           </>)  })}
     </>
   );
 }
