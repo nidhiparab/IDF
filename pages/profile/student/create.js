aea923eb650698489df420c35c107e6f9f495b0c
Khusheeta <khusheetaattarde11@gmail.com>
2023-01-29 15:27:28 +0530

    student create css

--- pages/profile/student/create.js
+++ pages/profile/student/create.js
@@ -36,60 +36,66 @@ const StudentRegistrationForm = () => {
 
   return (
     <form onSubmit={handleSubmit}>
-      <label>
+      <div className=" mx-auto p-20 items-center shadow-2xl shadow-slate-700 rounded-2xl w-2/3"  >
+   
+      <label className="mx-auto px-5 block text-sm font-medium mb-2">
         First Name:
-        <input
-          type="text"
+        <input className="  w-full py-3 px-4 my-2   border rounded-xl bg-slate-200 focus:outline-none border-none "
+          type="text" 
           value={firstName}
           onChange={(event) => setFirstName(event.target.value)}
         />
-      </label>
-      <br />
-      <label>
+      </label> 
+   
+
+ 
+
+      <label className=" px-5 block text-sm font-medium mb-2">
         Middle Name:
-        <input
-          type="text"
+        <input className="  w-full py-3 px-4  my-2   border rounded-xl bg-slate-200 focus:outline-none border-none "
+          type="text" 
           value={middleName}
           onChange={(event) => setMiddleName(event.target.value)}
         />
       </label>
-      <br />
-      <label>
+      
+      <label className=" px-5 block text-sm font-medium mb-2">
         Last Name:
-        <input
+        <input className="  w-full py-3 px-4 my-2   border rounded-xl bg-slate-200 focus:outline-none border-none "
           type="text"
+  
           value={lastName}
           onChange={(event) => setLastName(event.target.value)}
         />
       </label>
-      <br />
-      <label>
+    
+      <label className=" px-5 block text-sm font-medium mb-2">
        Gender:
-       <select value={gender} onChange={(event) => setGender(event.target.value)}>
+       <select   className="  w-full py-3 px-4 my-2border rounded-xl bg-slate-200 focus:outline-none border-none "value={gender} onChange={(event) => setGender(event.target.value)}>
        <option value="">Select a gender</option>
        <option value="Female">Female</option>
        <option value="male">Male</option>
        <option value="other">Other</option>
        </select>
       </label>
-      <br/>
-      <label>
+   
+      <label className="px-5 block text-sm font-medium mb-2">
         Date of Birth:
-        <input
+        <input className="  w-full py-3 px-4  my-2 border rounded-xl bg-slate-200 focus:outline-none border-none text-black"
           type="date"
           value={dob}
           onChange={(event) => setDob(event.target.value)}
         />
       </label>
-      <br />
-      <br />
-      <button type="submit">Register</button>
+    
+      <button className ="items-center  w-full bg-indigo-500 text-white py-2 px-4 my-2 rounded-md hover:bg-indigo-600" type="submit">Register</button></div>
     </form>
+    
   )
+  
 }
 
 export default StudentRegistrationForm
-
 
 
 
