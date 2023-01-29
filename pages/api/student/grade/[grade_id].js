5cdee2486cc4e26c6d057abbc84a7b3738369268
AdityaSawant0912 <arsawant09@gmail.com>
2023-01-29 12:59:51 +0530

    fixed grade id api

--- pages/api/student/grade/[grade_id].js
+++ pages/api/student/grade/[grade_id].js
@@ -1,70 +1,87 @@
 import executeQuery from '../../../../lib/db'
 import gradeConstants from '../../../../lib/grades';
 
-export default async function getGradeByStudentId(req, res) {
-  let result = {
-    grade_qualities:{},
-    grade_subjects:{},
-    grade_intrests:{},
-    grade_specifics:{},
-  }
-  const grade_id = req.query.grade_id;
-
-  let grade_qualities = await executeQuery({
-    query: "SELECT *, DATE_FORMAT(`timestamp`, '%d-%m-%Y') as timestamp FROM `grade_qualities` WHERE `grade_id` = ?;",
-    values: [grade_id]
-  })
-  grade_qualities = grade_qualities[0];
-  delete grade_qualities.grade_id;
-
-  for (const key in grade_qualities) {
-    if (Object.hasOwnProperty.call(grade_qualities, key)) {
-      const value = grade_qualities[key];
-      result.grade_qualities[gradeConstants.grade_qualities.columns[key]] = gradeConstants.grade_qualities.options[value];
-    }
-  }
+export default async function getGradeById(req, res) {
   
-  let grade_subjects = await executeQuery({
-    query: "SELECT * FROM `grade_subjects` WHERE `grade_id` = ?;",
+  let finalResult = [];
+  let { grade_id } = req.query;
+  let grades = await executeQuery({
+    query: "SELECT *, DATE_FORMAT(`timestamp`, '%d-%m-%Y') as timestamp FROM `grade` WHERE `grade_id` = ?;",
     values: [grade_id]
   })
-  grade_subjects = grade_subjects[0];
-  delete grade_subjects.grade_id;
+  console.log(grade_id);
 
-  for (const key in grade_subjects) {
-    if (Object.hasOwnProperty.call(grade_subjects, key)) {
-      const value = grade_subjects[key];
-      result.grade_subjects[gradeConstants.grade_subjects.columns[key]] = gradeConstants.grade_subjects.options[value];
+  for (const grade of grades) {
+
+    let result = {
+      grade_qualities: {},
+      grade_subjects: {},
+      grade_intrests: {},
+      grade_specifics: {},
     }
-  }
-  
-  let grade_intrests = await executeQuery({
-    query: "SELECT * FROM `grade_intrests` WHERE `grade_id` = ?;",
-    values: [grade_id]
-  })
-  grade_intrests = grade_intrests[0];
-  delete grade_intrests.grade_id;
 
-  for (const key in grade_intrests) {
-    if (Object.hasOwnProperty.call(grade_intrests, key)) {
-      const value = grade_intrests[key];
-      result.grade_intrests[gradeConstants.grade_intrests.columns[key]] = gradeConstants.grade_intrests.options[value];
+    result = {...grade, ...result}
+    
+    let grade_id = grade.grade_id;
+
+    let grade_qualities = await executeQuery({
+      query: "SELECT * FROM `grade_qualities` WHERE `grade_id` = ?;",
+      values: [grade_id]
+    })
+    grade_qualities = grade_qualities[0];
+    delete grade_qualities.grade_id;
+
+    for (const key in grade_qualities) {
+      if (Object.hasOwnProperty.call(grade_qualities, key)) {
+        const value = grade_qualities[key];
+        result.grade_qualities[gradeConstants.grade_qualities.columns[key]] = gradeConstants.grade_qualities.options[value];
+      }
     }
-  }
 
-  let grade_specifics = await executeQuery({
-    query: "SELECT * FROM `grade_specifics` WHERE `grade_id` = ?;",
-    values: [grade_id]
-  })
-  grade_specifics = grade_specifics[0];
-  delete grade_specifics.grade_id;
+    let grade_subjects = await executeQuery({
+      query: "SELECT * FROM `grade_subjects` WHERE `grade_id` = ?;",
+      values: [grade_id]
+    })
+    grade_subjects = grade_subjects[0];
+    delete grade_subjects.grade_id;
 
-  for (const key in grade_specifics) {
-    if (Object.hasOwnProperty.call(grade_specifics, key)) {
-      const value = grade_specifics[key];
-      result.grade_specifics[gradeConstants.grade_specifics.columns[key]] = value;
+    for (const key in grade_subjects) {
+      if (Object.hasOwnProperty.call(grade_subjects, key)) {
+        const value = grade_subjects[key];
+        result.grade_subjects[gradeConstants.grade_subjects.columns[key]] = gradeConstants.grade_subjects.options[value];
+      }
     }
+
+    let grade_intrests = await executeQuery({
+      query: "SELECT * FROM `grade_intrests` WHERE `grade_id` = ?;",
+      values: [grade_id]
+    })
+    grade_intrests = grade_intrests[0];
+    delete grade_intrests.grade_id;
+
+    for (const key in grade_intrests) {
+      if (Object.hasOwnProperty.call(grade_intrests, key)) {
+        const value = grade_intrests[key];
+        result.grade_intrests[gradeConstants.grade_intrests.columns[key]] = gradeConstants.grade_intrests.options[value];
+      }
+    }
+
+    let grade_specifics = await executeQuery({
+      query: "SELECT * FROM `grade_specifics` WHERE `grade_id` = ?;",
+      values: [grade_id]
+    })
+    grade_specifics = grade_specifics[0];
+    delete grade_specifics.grade_id;
+
+    for (const key in grade_specifics) {
+      if (Object.hasOwnProperty.call(grade_specifics, key)) {
+        const value = grade_specifics[key];
+        result.grade_specifics[gradeConstants.grade_specifics.columns[key]] = value;
+      }
+    }
+
+    finalResult.push(result);
   }
-  res.json({result })
+  res.json({result: finalResult })
 
 }
