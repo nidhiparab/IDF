b9df386ec2a4b62b19f1e9eb04707a3756ed097d
Nidhi <nidhiparabmail@gmail.com>
2023-01-30 10:37:47 +0530

    all students row

--- pages/profile/students.js
+++ pages/profile/students.js
@@ -17,10 +17,10 @@ const Students = ({ students }) => {
       {students.map(student => {
         return (<>
 
-          <div className="m-20 p-10 items-center shadow-2xl shadow-slate-700 rounded-2xl" key={student.student_id}>
-            <div className=' text-l'>
-              <div className='justify-between p-2'>
-                <h3 className='text-l font-bold text-blue-600  mt-auto mb-3'>Name</h3>
+          <div className="m-20 p-5 items-center shadow-2xl shadow-slate-700 rounded-2xl" key={student.student_id}>
+            <div className=' text-l flex flex-row justify-between'>
+              <div className='flex flex-col'>
+                <h3 className=' font-bold text-blue-600'>Name</h3>
                 <span className='mt-auto mb-2'> {student?.f_name} {student?.m_name} {student?.l_name}</span>
               </div>
               <div className=' my-auto w-auto flex flex-col items-start justify-between p-2'>
@@ -30,8 +30,8 @@ const Students = ({ students }) => {
               <div className=' my-auto w-auto flex flex-col items-start justify-between p-2'>
                 <h3 className='text-l font-bold text-blue-600 mt-auto mb-3'>Balgurukul</h3>
                 <Link href={`/balgurukul/${student.bg_id}`} className='mt-auto mb-2'>{student.bg_name}</Link>
-              </div>
-              <Link href={ `/profile/student/${student.student_id}` } className='btn btn-primary' > Details </Link>
+              </div >
+              <Link href={ `/profile/student/${student.student_id}` } className='btn btn-primary h-2/3' > Details </Link>
             </div>
           </div>
 
